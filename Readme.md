ede3263e8ae2dbf4c3636b4842954d52ce17b4c5
suhesong <937445794@qq.com>
2018-09-04 12:45:37 +0800

    完成readme

---
+++ Readme.md
@@ -0,0 +1,21 @@
+前端使用pug模板
+
+后端使用express框架
+
+数据库使用mongoDB
+
+使用MVC的结构
+
+controllers，存放控制层组件，即路由与执行方法
+
+models存放数据模型，本例中只有一条item
+
+views是视图层，存放pug模板
+
+public文件夹存放静态资源，upload与HTML是练习文件夹
+router是server.js的路由文件夹//练习使用
+
+打开本项目：
+    git clone
+    npm install
+    npm start
--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express=require('express');
-var todoController=require('./controllers/todoController');
+var express = require('express');
+var todoController = require('./controllers/todoController');
 
-var app=express();
+var app = express();
 
-app.set('view engine','pug');
+app.set('view engine', 'pug');
 
 app.use(express.static('./public'));
 
 todoController(app);
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('port 3000 open');
-})
+});
